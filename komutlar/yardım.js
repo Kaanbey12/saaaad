dbcff586fd1a46cc802fc8b85eb54cf3a3bb4a3f
Glitch (lightning-snowshoe) <none>
2021-07-05 07:26:09 +0000

    🚎🚓 Checkpoint ./komutlar/yardım.js:32467586/2225

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -1,47 +1,31 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-const db = require("quick.db");
-exports.run = (client, message, args, member) => {
-  const yardım = new Discord.MessageEmbed()
-    .setAuthor(`Twilight Yardım Menusü`, client.user.avatarURL())
-    .setColor("0x36393F")
-    .setThumbnail(client.user.avatarURL())
-    .setDescription(
-      `• Hey! <@${
-        message.author.id
-      }> beni kullandığın için teşekkür ederim!\n •  Prefixim: **!**\n • Dilim: **TR** :flag_tr:\n • Üyelik durumu: ${
-        db.has(`üyelikk_${message.author.id}`, "üyelik")
-          ? `**Gold üye!**`
-          : `**Normal üye!**`
-      }`
-    )
-    .addField(
-      " • Kategoriler:",
-      `> • [!kullanıcı : **Kullanıcı yardım menüsünü gösterir.**\n > • !moderasyon : **Moderasyon yardım menüsünü gösterir.**\n > • !kayıtsistemi : ** Kayıt sistemi yardım menüsünü gösterir.**\n > • !korumasistemi : Koruma sistemi yardım menüsünü gösterir.**\n > • !logosistemi : ** Logo sistemi yardım menüsünü gösterir.**\n > • !çekilişsistemi : **Çekiliş sistemi yardım menüsünü gösterir.** Geliştiricim Program Değişti Bu Yüz den Hatalar Vardır!`
-    )
-    .addField(
-      " • Güncelleme Notları:",
-      "**Güncelleme v0.4:** Çekiliş sistemi eklendi!"
-    )
+bot.command({
+  name:"yardım",
+  code:`$title[Yardım Menüm]
+$thumbnail[$userAvatar]
+$description[Hey Selam <@$authorID> Sanırım Komutlarıma Bakmak İstedin,
+Unutmaki Prefixim **!**
 
-    .addField(
-      " • Linkler:",
-      "• [Davet Et](https://discord.com/oauth2/authorize?client_id=849214555052834827&scope=bot&permissions=8) • [Destek Sunucusu](https://discord.gg/g7dWfHD6) • [Web Site](https://dcbotlar.xyz) •"`Geçersizdir!`
-    )
-    .setImage("")
-    .setFooter("Asreaper", message.author.avatarURL())
-    .setTimestamp();
-  message.channel.send(yardım);
-};
-exports.conf = {
-  enabled: true,
+:gear: **Moderasyon Komutlarıma Bakmak İçin**
+・**!yardım**
+
+:goggles: **Kullanıcı Komutlarıma Bakmak İçin**
+・**!kullanıcı**
+
+:rocket: **Eğlence Komutlarıma Bakmak İçin**
+・**!eğlence**
+
+:movie_camera: **Kayıt Sistemime Bakmak İçin**
+• **!kayıtsistemi**
+
+$color[$random[0;99999]]`
+  })
   guildOnly: false,
   aliases: ["y", "help", "h"],
-  permLevel: 0
-};
+  
 
-exports.help = {
-  name: "yardım",
-  description: "westra",
-  usage: "westra"
-};
+
+
+  
+ 
+  
+
